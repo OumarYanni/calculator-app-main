8c8a8f84f11a7f4edf82ab9d23efc50f3d7d64f1
Oumar Yanni <oumaryanni@gmail.com>
2023-10-24 08:01:03 +0000

    font-family and font-size configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: "League Spartan, sans-serif",
+      },
+      fontSize: {
+        "3xl": "2rem",
+      },
+    },
   },
   plugins: [],
 };
