3c7d970583145a0cfb81751f0ccc9252c3147366
Oumar Yanni <oumaryanni@gmail.com>
2023-10-26 00:39:51 +0000

    Theme configuration update and continued integration of the design.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
+const FIORD = "#3A4764";
+const MERCURY = "#e6e6e6";
+const VIOLET = "#160628";
+const EBONYCLAY = "#232C43";
+const SWISSCOFFEE = "#d1cccc";
+const VIOLET2 = "#1d0934";
+const MIRAGE = "#182034";
+const GALLERY = "#ededed";
+const LYNCH = "#637097";
+const PARADISO = "#377f86";
+const WINDSOR = "#58077d";
+const EASTBAY = "#404e72";
+const BLUMINE = "#1b5f65";
+const ELECTRICVIOLET = "#bc15f4";
+const PERSIANRED = "#d03f2f";
+const BURNTORANGE = "#ca5502";
+const ROBINSEGGBLUE = "#00e0d1";
+const OLDBRICK = "#93261a";
+const PERUTAN = "#893901";
+const AQUAMARINE = "#6cf9f2";
+const PEARLBUSH = "#eae3dc";
+const WESTAR = "#e5e4e1";
+const GRAPE = "#341c4f";
+const NAPA = "#b4a597";
+const ZORBA = "#a69d91";
+const SEANCE = "#871c9c";
+
+const RIVERBED = "#444b5a";
+const BRIGHTSUN = "#ffe53d";
+const SHARK = "#1b2428";
+const WHITE = "#ffffff";
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
@@ -10,37 +43,46 @@ module.exports = {
         "3xl": "2rem",
       },
       backgroundColor: {
-        "theme-1--very-dark-desaturated-blue-color": "#3a4764",
-        "theme-2--light-gray-color": "#e6e6e6",
-        "theme-3--very-dark-violet-color": "#160628",
-        "theme-1--very-dark-desaturated-blue-2-color": "#232c43",
-        "theme-2--grayish-red-color": "#d1cccc",
-        "theme-3--very-dark-violet-2-color": "#1d0934",
-        "theme-1-very-dark-desaturated-blue-3-color": "#182034",
-        "theme-2-very-light-gray-color": "#ededed",
-        "theme-1-desaturated-dark-blue-color": "#637097",
-        "theme-2-dark-moderate-cyan-color": "#377f86",
-        "theme-3-dark-violet-color": "#58077d",
-        "theme-1-desaturated-dark-blue-2-color": "#404e72",
-        "theme-2-very-dark-cyan-color": "#1b5f65",
-        "theme-3-vivid-magenta-color": "#bc15f4",
-        "theme-1-red-color": "#d03f2f",
-        "theme-2-orange-color": "#ca5502",
-        "theme-3-pure-cyan-color": "#00e0d1",
-        "theme-1-dark-red-color": "#93261a",
-        "theme-2-dark-orange-color": "#893901",
-        "theme-3-soft-cyan-color": "#6cf9f2",
-        "theme-1-light-grayish-orange-color": "#eae3dc",
-        "theme-2-light-grayish-yellow-color": "#e5e4e1",
-        "theme-3-very-dark-violet": "#341c4f",
-        "theme-1-grayish-orange-color": "#b4a597",
-        "theme-2-dark-grayish-orange-color": "#a69d91",
-        "theme-3-dark-magenta-color": "#871c9c",
+        fiord: FIORD,
+        mercury: MERCURY,
+        violet: VIOLET,
+        ebonyclay: EBONYCLAY,
+        swisscoffee: SWISSCOFFEE,
+        violet2: VIOLET2,
+        mirage: MIRAGE,
+        gallery: GALLERY,
+        lynch: LYNCH,
+        paradiso: PARADISO,
+        windsor: WINDSOR,
+        eastbay: EASTBAY,
+        blumine: BLUMINE,
+        electricviolet: ELECTRICVIOLET,
+        persianred: PERSIANRED,
+        burntorange: BURNTORANGE,
+        robinseggblue: ROBINSEGGBLUE,
+        oldbrick: OLDBRICK,
+        perutan: PERUTAN,
+        aquamarine: AQUAMARINE,
+        pearlbush: PEARLBUSH,
+        westar: WESTAR,
+        grape: GRAPE,
+        napa: NAPA,
+        zorba: ZORBA,
+        seance: SEANCE,
       },
       colors: {
-        "text-light-yellow-color": "#ffe53d",
-        "text-very-dark-blue-color": "#1b2428",
-        "text-white-color": "#ffffff",
+        riverbed: RIVERBED,
+        brightsun: BRIGHTSUN,
+        shark: SHARK,
+        white: WHITE,
+      },
+      boxShadow: {
+        keyboard: "0 0.3rem #b4a597",
+        keyboard2: "0 0.3rem #404e72",
+        keyboard3: "0 0.3rem #93261a",
+      },
+      height: {
+        inherit: "inherit",
       },
     },
   },
