5d11e74a824edd74158ca7f28605dee4afa21249
Oumar Yanni <oumaryanni@gmail.com>
2023-10-30 23:10:57 +0000

    Primary key-number activation and display on calculator screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import "./input.css";
+
+const result = document.querySelector(".result");
+
+function updateScreen(number) {
+  result.innerText = number;
+}
+
+const keysNumber = document
+  .querySelectorAll(".key-number")
+  .forEach((key, index) => {
+    key.addEventListener("click", () => {
+      const number = parseInt(key.innerText, 10);
+      const result = number;
+
+      updateScreen(result);
+      //console.log(key.innerText);
+      //console.log(parseInt(key.innerText));
+
+      //const typeKeyNumber = typeof key.innerText;
+      //console.log(typeKeyNumber);
+      console.log(index);
+      console.log(number);
+      const typeKeyNumber = typeof number;
+      console.log(typeKeyNumber);
+    });
+  });
